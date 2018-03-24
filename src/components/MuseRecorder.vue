b805e6422635f6b1b66f79d07a6ca8a8bf0f163b
David Nowinsky <david@nowinsky.net>
2018-03-24 11:46:31 +0100

    Create recordings averaged on 10s every 1s

--- src/components/MuseRecorder.vue
+++ src/components/MuseRecorder.vue
@@ -1,26 +1,58 @@
 <template>
-  <div class="muse-recorder__controls">
-    <div
-      v-if="connectionStatus"
-    > Connected to muse
+  <div class="muse-recorder">
+
+    <div class="muse-recorder__connection">
+      <div
+        v-if="connectionStatus"
+      > Connected to muse
+      </div>
+
+      <button
+        v-else
+        @click="connectToMuse()"
+      > Connect
+      </button>
     </div>
 
-    <button
-      v-else
-      @click="connectToMuse()"
-    > Connect
-    </button>
+    <div class="muse-recorder__recordings">
+      <button
+        v-if="connectionStatus && !recordingInProgress"
+        @click="startRecording()"
+      > Start a new recording
+      </button>
+      <button
+        v-if="recordingInProgress"
+        @click="stopRecording()"
+      > Stop the recording
+      </button>
+      <pre>
+        {{ currentRecordingDisplay }}
+      </pre>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Inject, Vue } from 'vue-property-decorator';
 import { MuseClient } from 'muse-js';
+import { EEGRelativePowerBand, POWER_BANDS, FrequencyBand } from 'muse-js/dist/lib/process-samples';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { bufferCount, map } from 'rxjs/operators';
+
+interface AveragedRelativeBandPowers {
+  [index: string]: number;
+}
 
 @Component
 export default class MuseRecorder extends Vue {
   private museClient: MuseClient;
   private connectionStatus: boolean = false;
+  private recordingInProgress: boolean = false;
+  private currentRecordingSubscription: Subscription;
+
+  private currentRecording: AveragedRelativeBandPowers[] = [];
 
   private created() {
     this.museClient = new MuseClient();
@@ -33,6 +65,58 @@ export default class MuseRecorder extends Vue {
     this.museClient.connectionStatus.subscribe(
       cS => this.connectionStatus = cS
     );
+  }
+
+  private startRecording() {
+    this.currentRecording = [];
+    this.currentRecordingSubscription = (this.museClient.relativeBandPowers as Observable<EEGRelativePowerBand[]>)
+      .pipe(
+        bufferCount(100, 10)
+      ,
+        map( (recordings: EEGRelativePowerBand[][]): EEGRelativePowerBand[] =>
+          Object.keys(POWER_BANDS).map( (k): EEGRelativePowerBand => {
+            return {
+              band: POWER_BANDS[k],
+              power: recordings.map(
+                  (recording: EEGRelativePowerBand[]): number => {
+                    let bandRecording = recording.find( r => r.band.id == POWER_BANDS[k].id );
+                    if (bandRecording) {
+                      return bandRecording.power;
+                    } else {
+                      return NaN;
+                    }
+                  }
+                ).reduce(
+                  ((sumOfRecordings, bandRecordingValue) =>
+                    sumOfRecordings += bandRecordingValue
+                  ), 0
+                ) / recordings.length
+            };
+          })
+        )
+      ,
+        map( (rbps: EEGRelativePowerBand[]): AveragedRelativeBandPowers => {
+          let arbps: AveragedRelativeBandPowers = {};
+          rbps.forEach( (rbp) => arbps[rbp.band.id] = rbp.power );
+          return arbps;
+        })
+      ).subscribe(
+        (arbps) => this.currentRecording.push(arbps)
+      );
+    this.recordingInProgress = true;
+  }
+
+  private stopRecording() {
+    this.currentRecordingSubscription.unsubscribe();
+    this.recordingInProgress = false;
+  }
+
+  get currentRecordingDisplay() {
+    return this.currentRecording;
+  }
+
+  private beforeDestroy() {
+    this.stopRecording();
   }
 }
 </script>
